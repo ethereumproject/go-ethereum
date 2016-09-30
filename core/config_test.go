3ac8b01f254ad7100c97ac53e66c17a3aa13493c
Elaine Ou <elaine.ou@gmail.com>
2016-09-30 12:52:20 -0700

    test configs for bomb delay

---
+++ core/config_test.go
@@ -0,0 +1,95 @@
+package core
+
+import (
+	"math/big"
+	"testing"
+)
+
+func TestBombWait(t *testing.T) {
+	config := NewChainConfig()
+
+	if (config.IsHomestead(big.NewInt(10000))) {
+		t.Errorf("Unexpected for %d", 10000)
+	}
+
+	if (!config.IsHomestead(big.NewInt(1920000))) {
+		t.Errorf("Expected for %d", 1920000)
+	}
+	if (!config.IsHomestead(big.NewInt(2325166))) {
+		t.Errorf("Expected for %d", 2325166)
+	}
+	if (!config.IsHomestead(big.NewInt(3000000))) {
+		t.Errorf("Expected for %d", 3000000)
+	}
+	if (!config.IsHomestead(big.NewInt(3000001))) {
+		t.Errorf("Expected for %d", 3000001)
+	}
+	if (!config.IsHomestead(big.NewInt(4000000))) {
+		t.Errorf("Expected for %d", 3000000)
+	}
+	if (!config.IsHomestead(big.NewInt(5000000))) {
+		t.Errorf("Unexpected for %d", 5000000)
+	}
+	if (!config.IsHomestead(big.NewInt(5000001))) {
+		t.Errorf("Unexpected for %d", 5000001)
+	}
+}
+
+func TestBombDelay(t *testing.T) {
+	config := NewChainConfig()
+
+	if (config.IsDiehard(big.NewInt(1920000))) {
+		t.Errorf("Unexpected for %d", 1920000)
+	}
+
+	if (config.IsDiehard(big.NewInt(2325166))) {
+		t.Errorf("Unexpected for %d", 2325166)
+	}
+
+	if (!config.IsDiehard(big.NewInt(3000000))) {
+		t.Errorf("Expected for %d", 3000000)
+	}
+	if (!config.IsDiehard(big.NewInt(3000001))) {
+		t.Errorf("Expected for %d", 3000001)
+	}
+	if (!config.IsDiehard(big.NewInt(4000000))) {
+		t.Errorf("Expected for %d", 3000000)
+	}
+
+	if (config.IsDiehard(big.NewInt(5000000))) {
+		t.Errorf("Unexpected for %d", 5000000)
+	}
+	if (config.IsDiehard(big.NewInt(5000001))) {
+		t.Errorf("Unexpected for %d", 5000001)
+	}
+}
+
+func TestBombExplode(t *testing.T) {
+	config := NewChainConfig()
+
+	if (config.IsExplosion(big.NewInt(1920000))) {
+		t.Errorf("Unexpected for %d", 1920000)
+	}
+
+	if (config.IsExplosion(big.NewInt(2325166))) {
+		t.Errorf("Unexpected for %d", 2325166)
+	}
+
+	if (config.IsExplosion(big.NewInt(3000000))) {
+		t.Errorf("Unxpected for %d", 3000000)
+	}
+	if (config.IsExplosion(big.NewInt(3000001))) {
+		t.Errorf("Unxpected for %d", 3000001)
+	}
+	if (config.IsExplosion(big.NewInt(4000000))) {
+		t.Errorf("Unxpected for %d", 3000000)
+	}
+
+	if (!config.IsExplosion(big.NewInt(5000000))) {
+		t.Errorf("Expected for %d", 5000000)
+	}
+	if (!config.IsExplosion(big.NewInt(5000001))) {
+		t.Errorf("Expected for %d", 5000001)
+	}
+
+}
