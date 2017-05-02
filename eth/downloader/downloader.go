663cb74c01c01101e191c6fb51014c9923c4901a
Mr. Is <isaac.ardis@gmail.com>
2017-05-02 16:56:30 -0500

    problem: 'sync busy' logging was annoying and misleading solution: log only to debugger

--- eth/downloader/downloader.go
+++ eth/downloader/downloader.go
@@ -299,7 +299,7 @@ func (d *Downloader) Synchronise(id string, head common.Hash, td *big.Int, mode 
 		return true
 
 	case errBusy:
-		log.Print("sync busy")
+		glog.V(logger.Debug).Info("sync busy")
 
 	case errTimeout, errBadPeer, errStallingPeer, errEmptyHashSet,
 		errEmptyHeaderSet, errPeersUnavailable, errTooOld,
