9c38e25843a115ae9875c7311e5bb13882c4d658
Mr. Is <isaac.ardis@gmail.com>
2017-05-22 15:02:24 -0500

    Turn off watcher for cachedb and use go routines to handle iteration load

--- accounts/cachedb.go
+++ accounts/cachedb.go
@@ -32,6 +32,9 @@ import (
 	"bytes"
 	"errors"
 	"sync"
+	"github.com/mailru/easyjson"
+	"fmt"
+	"runtime"
 )
 
 var addrBucketName = []byte("byAddr")
@@ -92,7 +95,7 @@ func newCacheDB(keydir string) *cacheDB {
 		}
 	}
 	
-	cdb.watcher = newWatcher(cdb)
+	//cdb.watcher = newWatcher(cdb)
 	return cdb
 }
 
@@ -265,32 +268,32 @@ func (cdb *cacheDB) find(a Account) (Account, error) {
 
 func (cdb *cacheDB) close() {
 	cdb.mu.Lock()
-	cdb.watcher.close()
-	if cdb.throttle != nil {
-		cdb.throttle.Stop()
-	}
+	//cdb.watcher.close()
+	//if cdb.throttle != nil {
+	//	cdb.throttle.Stop()
+	//}
 	cdb.db.Close()
 	cdb.mu.Unlock()
 }
 
 func (cdb *cacheDB) maybeReload() {
-	cdb.mu.Lock()
-	defer cdb.mu.Unlock()
-	if cdb.watcher.running {
-		return // A watcher is running and will keep the cache up-to-date.
-	}
-	if cdb.throttle == nil {
-		cdb.throttle = time.NewTimer(0)
-	} else {
-		select {
-		case <-cdb.throttle.C:
-		default:
-			return // The cache was reloaded recently.
-		}
-	}
-	cdb.watcher.start()
+	//cdb.mu.Lock()
+	//defer cdb.mu.Unlock()
+	//if cdb.watcher.running {
+	//	return // A watcher is running and will keep the cache up-to-date.
+	//}
+	//if cdb.throttle == nil {
+	//	cdb.throttle = time.NewTimer(0)
+	//} else {
+	//	select {
+	//	case <-cdb.throttle.C:
+	//	default:
+	//		return // The cache was reloaded recently.
+	//	}
+	//}
+	//cdb.watcher.start()
 	cdb.reload()
-	cdb.throttle.Reset(minReloadInterval)
+	//cdb.throttle.Reset(minReloadInterval)
 }
 
 func (cdb *cacheDB) setLastUpdated() error {
@@ -360,25 +363,15 @@ func (cdb *cacheDB) setBatchAccounts(accs []Account) (errs []error) {
 // Callers must hold ac.mu.
 func (cdb *cacheDB) reload() {
 	defer cdb.setLastUpdated()
-	cdb.Syncfs2db(time.Now().Add(-minReloadInterval))
+	cdb.Syncfs2db(time.Now().Round(time.Second).Add(-minReloadInterval))
 }
 
 // Syncfs2db syncronises an existing cachedb with a corresponding fs.
 func (cdb *cacheDB) Syncfs2db(lastUpdated time.Time) (errs []error) {
 	defer cdb.setLastUpdated()
 
-	files, err := ioutil.ReadDir(cdb.getKeydir())
-	if err != nil {
-		return append(errs, err)
-	}
-
 	var (
-		buf     = new(bufio.Reader)
 		accounts []Account
-		keyJSON struct {
-			Address common.Address `json:"address"`
-		}
-		web3JSON []byte
 		removedKeyAddrsFiles [][]byte
 		removedKeyFiles []string
 	)
@@ -386,7 +379,13 @@ func (cdb *cacheDB) Syncfs2db(lastUpdated time.Time) (errs []error) {
 	// SYNC: DB --> FS.
 	// Iterate addrFiles and touch all in FS, so ensure have "updated" all files which are present in db.
 	// Any _new_ files will not have been touched.
-	n := time.Now()
+
+	// Time rounded down to second because mod times are store with second accuracy.
+	entryCounter := 0
+	n := time.Now().Round(time.Second).Add(-minReloadInterval)
+
+	waiterTouch := &sync.WaitGroup{}
+
 	e := cdb.db.Update(func (tx *bolt.Tx) error {
 
 		ab := tx.Bucket(addrBucketName)
@@ -395,6 +394,7 @@ func (cdb *cacheDB) Syncfs2db(lastUpdated time.Time) (errs []error) {
 		c := ab.Cursor()
 
 		for k, v := c.First(); k != nil; k, v = c.Next() {
+			entryCounter++
 
 			// Has address prefix.
 			// 0x3ce3a47893f187b0e9f9b2b7e8bdf9052b8e6968UTC--2017-05-19T19-32-22.434229436Z--3ce3a47893f187b0...
@@ -406,6 +406,7 @@ func (cdb *cacheDB) Syncfs2db(lastUpdated time.Time) (errs []error) {
 
 				// Get just the file name.
 				fp = fp[42:]
+				glog.V(logger.Debug).Infof("(%v) Checking key: %v", entryCounter, fp)
 
 				// No file available as suffix to account address.
 				if fp == "" {
@@ -421,45 +422,52 @@ func (cdb *cacheDB) Syncfs2db(lastUpdated time.Time) (errs []error) {
 				// Key includes file name.
 				} else {
 					// /home/data-dir/mainnet/keystore/UTC--2017-05-19T19-32-22.434229436Z--3ce3a47893f187b0...
-					p := filepath.Join(cdb.getKeydir(), fp)
+					p := filepath.Join(cdb.keydir, fp)
+					waiterTouch.Add(1)
+					go func (path string, rkf []string, waiter *sync.WaitGroup) {
+						defer waiter.Done()
+						fi, e := os.Stat(p)
+						// File exists. Check modification time.
+						if e == nil {
 
-					fi, e := os.Stat(p)
-					// File exists. Check modification time.
-					if e == nil {
+							// Only touch files that haven't been modified since entry was updated
+							entryTime, te := time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", string(v))
+							if te != nil {
+								errs = append(errs, e)
+							} else {
+								// DB has been updated more recently than file.
+								if entryTime.After(fi.ModTime()) {
 
-						// Only touch files that haven't been modified since entry was updated
-						tt, te := time.Parse("2006-01-02 15:04:05.999999999 -0700 MST", string(v))
-						if te != nil {
-							errs = append(errs, te)
-						} else {
-							// DB has been updated more recently than file.
-							if tt.After(fi.ModTime()) {
-
-								// FIXME: is there a better way to `touch`?
-								if cherr := os.Chtimes(p, n, n); cherr != nil {
-									errs = append(errs, cherr)
+									// FIXME: `touch`; is there a better way?
+									if cherr := os.Chtimes(p, n, n); cherr != nil {
+										errs = append(errs, cherr)
+									}
 								}
 							}
 						}
-					}
-					// This account file has been removed.
-					if e != nil && os.IsNotExist(e) {
-						removedKeyFiles = append(removedKeyFiles, fp)
-					} else {
-						errs = append(errs, err)
-						continue
-					}
+						// This account file has been removed.
+						if e != nil && os.IsNotExist(e) {
+							glog.V(logger.Debug).Infof("Found key to remove from db: %v", fp)
+							removedKeyFiles = append(removedKeyFiles, fp)
+						} else {
+							errs = append(errs, e)
+						}
+					}(p, removedKeyFiles, waiterTouch)
 				}
 			}
 		}
 
+		waiterTouch.Wait()
+
 		// Remove from both caches.
 		for _, kaf := range removedKeyAddrsFiles {
+			glog.V(logger.Debug).Infof("removing key addr file from cachedb: %v", kaf)
 			if e := ab.Delete(kaf); e != nil {
 				errs = append(errs, e)
 			}
 		}
 		for _, kf := range removedKeyFiles {
+			glog.V(logger.Debug).Infof("removing key file from cachedb: %v", kf)
 			if e := fb.Delete([]byte(kf)); e != nil {
 				errs = append(errs, e)
 			}
@@ -470,110 +478,182 @@ func (cdb *cacheDB) Syncfs2db(lastUpdated time.Time) (errs []error) {
 		errs = append(errs, e)
 	}
 
+	files, err := ioutil.ReadDir(cdb.keydir)
+	if err != nil {
+		return append(errs, err)
+	}
+	numFiles := len(files)
+
+	glog.V(logger.Debug).Infof("Syncing index db: %v files", numFiles)
+
+	waitUp := &sync.WaitGroup{}
+	achan := make(chan Account)
+	echan := make(chan error)
+	done := make(chan bool, 1)
+
 	// SYNC: FS --> DB.
+
+	// Handle receiving errors/accounts.
+	go func() {
+		for j := 0; j < len(files); j++ {
+			select {
+			case a := <-achan:
+				//glog.V(logger.Debug).Infof("[%v/%v] got account from channel: %v", j, len(files), a)
+				if (a == Account{}) {
+					continue
+				}
+				accounts = append(accounts, a)
+				if len(accounts) == 5000 {
+					if e := cdb.setBatchAccounts(accounts); len(e) != 0 {
+						for _, ee := range e {
+							if ee != nil {
+								errs = append(errs, e...)
+							}
+						}
+					}
+					accounts = nil
+				}
+			case e := <-echan:
+				//glog.V(logger.Debug).Infof("[%v/%v] got err from channel: %v", j, len(files), e)
+				if e != nil {
+					errs = append(errs, e)
+				}
+			}
+		}
+	}()
+
+	// Iterate files.
 	for i, fi := range files {
 
 		// fi.Name() is used for storing the file in the case db
 		// This assumes that the keystore/ dir is not designed to walk recursively.
 		// See testdata/keystore/foo/UTC-afd..... compared with cacheTestAccounts for
 		// test proof of this assumption.
-		path := filepath.Join(cdb.getKeydir(), fi.Name())
+		path := filepath.Join(cdb.keydir, fi.Name())
 		if e != nil {
 			errs = append(errs, e)
 		}
-
-		if skipKeyFile(fi) {
-			glog.V(logger.Detail).Infof("ignoring file %s", path)
-
+		waitUp.Add(1)
+		if runtime.NumGoroutine() > 500 {
+			processKeyFile(waitUp, path, fi, i, n, numFiles, achan, echan)
 		} else {
-			// Check touch time from above iterator.
-			newy := false
-			if fi, fe := os.Stat(path); fe == nil {
-				// newy == mod time is before n because we just touched the files we have indexed
-				if fi.ModTime().UTC().Before(n) {
-					newy = true
-				}
-			} else if fe != nil {
-				errs = append(errs, fe)
-				continue
-			}
-
-			if newy {
-				fd, err := os.Open(path)
-				if err != nil {
-					errs = append(errs, err)
-				}
-				buf.Reset(fd)
-				// Parse the address.
-				keyJSON.Address = common.Address{}
-				err = json.NewDecoder(buf).Decode(&keyJSON)
-				fd.Close()
-
-				web3JSON, err = ioutil.ReadFile(path)
-				if err != nil {
-					errs = append(errs, err)
-				}
-
-				switch {
-				case err != nil:
-					glog.V(logger.Debug).Infof("can't decode key %s: %v", path, err)
-				case (keyJSON.Address == common.Address{}):
-					glog.V(logger.Debug).Infof("can't decode key %s: missing or zero address", path)
-				default:
-					accounts = append(accounts, Account{Address: keyJSON.Address, File: fi.Name(), EncryptedKey: string(web3JSON)})
-				}
-			}
+			go processKeyFile(waitUp, path, fi, i, n, numFiles, achan, echan)
 		}
+	}
 
-		// Stash a batch or finish up.
-		if (len(accounts) == 10000) || (i == len(files) - 1 ) {
-			if e := cdb.setBatchAccounts(accounts); len(e) != 0 {
-				for _, ee := range e {
-					if ee != nil {
-						errs = append(errs, e...)
-					}
-				}
+	// Cleanup.
+	go func(wg *sync.WaitGroup, achan chan Account, echan chan error) {
+		waitUp.Wait()
+		close(achan)
+		close(echan)
+		done <- true
+	}(waitUp, achan, echan)
+	<-done
 
-			} else {
-				accounts = nil
+	if e := cdb.setBatchAccounts(accounts); len(e) != 0 {
+		for _, ee := range e {
+			if ee != nil {
+				errs = append(errs, e...)
 			}
 		}
 	}
+	accounts = nil
+
+	for _, e := range errs {
+		if e != nil {
+			glog.V(logger.Debug).Infof("Error: %v", e)
+		}
+	}
+
 	return errs
 }
 
-func bytesToAccount(bs []byte) Account {
-	aux := &struct{
-		Address string `json:"address"`
-		EncryptedKey string `json:"key"`
-		File string `json:"file"`
-	}{}
-	if e := json.Unmarshal(bs, aux); e != nil {
-		//panic(e)
-		return Account{}
+func processKeyFile(wg *sync.WaitGroup, path string, fi os.FileInfo, i int, n time.Time, numFiles int, aChan chan Account, errs chan error) {
+	defer wg.Done()
+	if skipKeyFile(fi) {
+		glog.V(logger.Debug).Infof("(%v/%v) Ignoring file %s", i, numFiles, fi.Name())
+		errs <- nil
+		return
+	} else {
+		// Check touch time from above iterator.
+		newy := false
+		if fi, fe := os.Stat(path); fe == nil {
+			// newy == mod time is before n because we just touched the files we have indexed
+			if fi.ModTime().Before(n) {
+				newy = true
+			}
+		} else if fe != nil {
+			// just track the error
+			errs <- fe
+			return
+		}
+		if !newy {
+			glog.V(logger.Debug).Infof("(%v, %v) File existing in index, skipping: %v", i, numFiles, fi.Name())
+			errs <- nil
+			return
+		} else {
+			glog.V(logger.Debug).Infof("(%v/%v) Adding key file to db: %v", i, numFiles, fi.Name())
+
+			keyJSON := struct {
+				Address common.Address `json:"address"`
+			}{}
+
+			buf := new(bufio.Reader)
+			fd, err := os.Open(path)
+			if err != nil {
+				errs <- err
+				return
+			}
+			buf.Reset(fd)
+			// Parse the address.
+			keyJSON.Address = common.Address{}
+			err = json.NewDecoder(buf).Decode(&keyJSON)
+			fd.Close()
+
+			web3JSON := []byte{}
+			web3JSON, err = ioutil.ReadFile(path)
+			if err != nil {
+				errs <- err
+				return
+			}
+
+			switch {
+			case err != nil:
+				glog.V(logger.Debug).Infof("(%v/%v) can't decode key %s: %v", i, numFiles, path, err)
+				errs <- err
+			case (keyJSON.Address == common.Address{}):
+				glog.V(logger.Debug).Infof("(%v/%v) can't decode key %s: missing or zero address", i, numFiles, path)
+				errs <- fmt.Errorf("(%v/%v) can't decode key %s: missing or zero address", i, numFiles, path)
+			default:
+				aChan <- Account{Address: keyJSON.Address, File: fi.Name(), EncryptedKey: string(web3JSON)}
+			}
+		}
 	}
-	a := Account{
+}
+
+func bytesToAccount(bs []byte) Account {
+	var aux AccountJSON
+	if e := easyjson.Unmarshal(bs, &aux); e != nil {
+		panic(e)
+		//return Account{}
+	}
+	return Account{
 		Address: common.HexToAddress(aux.Address),
 		EncryptedKey: aux.EncryptedKey,
 		File: aux.File,
 	}
-	return a
 }
 
 func accountToBytes(account Account) []byte {
-	aux := &struct{
-		Address string `json:"address"`
-		EncryptedKey string `json:"key"`
-		File string `json:"file"`
-	}{
+	aux := &AccountJSON{
 		Address: account.Address.Hex(),
 		EncryptedKey: account.EncryptedKey,
 		File: account.File,
 	}
-	b, e := json.Marshal(aux)
+	b, e := easyjson.Marshal(aux)
 	if e != nil {
-		return nil
-		//panic(e)
+		panic(e)
+		// return nil
 	}
 	return b
 }
