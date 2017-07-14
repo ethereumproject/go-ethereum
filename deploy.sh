ff0f39cf2426ca1d9a077ef793b0cf23f031b6cf
ia <isaac.ardis@gmail.com>
2017-07-14 09:41:09 -0500

    problem: janus arguments parsed incorrectly
    solution: use quotes around string arguments

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 mkdir ./deploy
 mv ./dist/*.tar.gz ./dist/*.zip ./deploy/
-janus deploy -to builds.etcdevteam.com/go-ethereum/$(janus version -format %M.%m.x) -files ./deploy/* -key ./gcloud-travis.json.enc
+janus deploy -to="builds.etcdevteam.com/go-ethereum/$(janus version -format %M.%m.x)" -files="./deploy/*" -key="./gcloud-travis.json.enc"
