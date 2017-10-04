7077b357e634524c2c9f6bdc89f79331d47fb27c
Tomasz Zdybał <tomek@zdybal.lap.pl>
2017-10-04 23:54:19 +0200

    Single struct for JSON-RPC response.
    This resolves issue #355.

--- rpc/server_test.go
+++ rpc/server_test.go
@@ -125,7 +125,7 @@ func testServerMethodExecution(t *testing.T, method string) {
 		t.Fatal(err)
 	}
 
-	response := JSONSuccessResponse{Result: &Result{}}
+	response := JSONResponse{Result: &Result{}}
 	if err := in.Decode(&response); err != nil {
 		t.Fatal(err)
 	}
