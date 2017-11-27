beeb31e408c3dc81d70618987b7d6917947b5c21
isaac.ardis@gmail.com <isaac.ardis@gmail.com>
2017-11-27 15:55:25 +0700

    Revert "sketch fuck with initsync vs catchupsync, drop ttl scaling to 1:rtt"
    This reverts commit a7381ce457091c5f628a9e4cd9a3b814fef8e56f.

--- eth/downloader/types.go
+++ eth/downloader/types.go
@@ -66,9 +66,6 @@ type chainRollbackFn func([]common.Hash)
 // peerDropFn is a callback type for dropping a peer detected as malicious.
 type peerDropFn func(id string)
 
-// pmIsSynced checks if the pm has finished initial sync
-type pmSyncedFn func() bool
-
 // dataPack is a data message returned by a peer for some query.
 type dataPack interface {
 	PeerId() string
