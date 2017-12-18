53fa808342698a62b6bba97670758fce2f1cfb56
Tomasz Zdybał <tomek@zdybal.lap.pl>
2017-12-18 14:59:25 +0100

    solution: ensure that gzipped log file is fsync'ed before removing plain log

--- logger/glog/glog.go
+++ logger/glog/glog.go
@@ -1044,6 +1044,9 @@ func gzipFile(name string) error {
 	if err = writer.Flush(); err != nil {
 		return err
 	}
+	if err = gzipped.Sync(); err != nil {
+		return err
+	}
 	if err = gzipped.Close(); err != nil {
 		return err
 	}
