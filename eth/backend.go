ab9494f6d851e5bc2fb2710164e6f723c716af45
ia <isaac.ardis@gmail.com>
2018-01-16 16:53:35 +0900

    problem: should allow chainID to be more flexible
    solution: allow eip155/chainID chain config val to be set
    on any fork, or not at all for custom configurations.
    
    resolves #471

--- eth/backend.go
+++ eth/backend.go
@@ -154,7 +154,7 @@ func New(ctx *node.ServiceContext, config *Config) (*Ethereum, error) {
 	}
 
 	glog.V(logger.Info).Infof("Protocol Versions: %v, Network Id: %v, Chain Id: %v", ProtocolVersions, config.NetworkId, config.ChainConfig.GetChainID())
-	glog.D(logger.Warn).Infof("Protocol Versions: %v, Network Id: %v, Chain Id: %v", logger.ColorGreen(fmt.Sprintf("%v", ProtocolVersions)), logger.ColorGreen(strconv.Itoa(config.NetworkId)), logger.ColorGreen(config.ChainConfig.GetChainID().String()))
+	glog.D(logger.Warn).Infof("Protocol Versions: %v, Network Id: %v, Chain Id: %v", logger.ColorGreen(fmt.Sprintf("%v", ProtocolVersions)), logger.ColorGreen(strconv.Itoa(config.NetworkId)), logger.ColorGreen(func() string { cid := config.ChainConfig.GetChainID().String(); if cid == "0" { cid = "not set" }; return cid }()))
 
 	// Load up any custom genesis block if requested
 	if config.Genesis != nil {
