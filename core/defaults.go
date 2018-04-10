aa1f1ab94403a314dabd743be56ee24b51394f49
Tomasz Zdybał <tomek@zdybal.lap.pl>
2018-04-10 15:23:39 +0200

    solution: special asset path processing (Windows)

--- core/defaults.go
+++ core/defaults.go
@@ -1,11 +1,6 @@
 package core
 
 import (
-	"fmt"
-	"io"
-	"strings"
-
-	"github.com/ethereumproject/go-ethereum/core/assets"
 	"github.com/ethereumproject/go-ethereum/logger/glog"
 )
 
@@ -18,21 +13,11 @@ func init() {
 
 	var err error
 
-	open := func(path string) (io.ReadCloser, error) {
-		// get rid of '\' that are results of filepath.Join on Windows
-		path = strings.Replace(path, "\\", "/", -1)
-
-		file, err := assets.DEFAULTS.Open(path)
-		if err != nil {
-			err = fmt.Errorf("Error opening '%s' default JSON: %v", path, err)
-		}
-		return file, err
-	}
-	DefaultConfigMainnet, err = parseExternalChainConfig("/core/config/mainnet.json", open)
+	DefaultConfigMainnet, err = parseExternalChainConfig("/core/config/mainnet.json", assetsOpen)
 	if err != nil {
 		glog.Fatal("Error parsing mainnet defaults from JSON:", err)
 	}
-	DefaultConfigMorden, err = parseExternalChainConfig("/core/config/morden.json", open)
+	DefaultConfigMorden, err = parseExternalChainConfig("/core/config/morden.json", assetsOpen)
 	if err != nil {
 		glog.Fatal("Error parsing morden defaults from JSON:", err)
 	}
