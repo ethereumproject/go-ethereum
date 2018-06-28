0b61fdbd0bcb28e531ea77b9bf02d315092dc9c0
r8d8 <ckryvomaz@gmail.com>
2018-06-28 17:06:23 +0300

    Fix merge conflict

--- core/assets/assets.go
+++ core/assets/assets.go
@@ -438,7 +438,11 @@ func init() {
 					modTime: time.Unix(0, 1525956628000000000),
 					isDir:   false,
 				},
+<<<<<<< HEAD:core/assets/assets.go
 			}, "/core/config/mainnet_bootnodes.json": {
+=======
+			}, "/params/config/mainnet_bootnodes.json": File{
+>>>>>>> 5f778f86e... Fix merge conflict:params/assets/assets.go
 				data: []byte{
 					0x7b, 0x0a, 0x20, 0x20, 0x20, 0x20, 0x22, 0x62, 0x6f, 0x6f, 0x74, 0x73,
 					0x74, 0x72, 0x61, 0x70, 0x22, 0x3a, 0x20, 0x5b, 0x0a, 0x20, 0x20, 0x20,
@@ -590,7 +594,11 @@ func init() {
 					modTime: time.Unix(0, 1525956628000000000),
 					isDir:   false,
 				},
+<<<<<<< HEAD:core/assets/assets.go
 			}, "/core/config/mainnet_genesis.json": {
+=======
+			}, "/params/config/mainnet_genesis.json": File{
+>>>>>>> 5f778f86e... Fix merge conflict:params/assets/assets.go
 				data: []byte{
 					0x7b, 0x0a, 0x09, 0x22, 0x67, 0x65, 0x6e, 0x65, 0x73, 0x69, 0x73, 0x22,
 					0x3a, 0x20, 0x7b, 0x0a, 0x09, 0x09, 0x22, 0x6e, 0x6f, 0x6e, 0x63, 0x65,
@@ -626,7 +634,11 @@ func init() {
 					modTime: time.Unix(0, 1527228759000000000),
 					isDir:   false,
 				},
+<<<<<<< HEAD:core/assets/assets.go
 			}, "/core/config/mainnet_genesis_alloc.csv": {
+=======
+			}, "/params/config/mainnet_genesis_alloc.csv": File{
+>>>>>>> 5f778f86e... Fix merge conflict:params/assets/assets.go
 				data: []byte{
 					0x22, 0x30, 0x30, 0x30, 0x64, 0x38, 0x33, 0x36, 0x32, 0x30, 0x31, 0x33,
 					0x31, 0x38, 0x65, 0x63, 0x36, 0x38, 0x39, 0x39, 0x61, 0x36, 0x37, 0x35,
