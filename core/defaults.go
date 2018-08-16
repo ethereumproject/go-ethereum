554609d527b4e35739539125a761b9c678798773
ia <isaac.ardis@gmail.com>
2018-08-16 15:40:04 -0500

    problem: tests/BlockchainTests want dedicated chain configs
    solution: create dedicated ChainConfigs for use by these tests
    
    - compiles core/config/assets

--- core/defaults.go
+++ core/defaults.go
@@ -5,8 +5,13 @@ import (
 )
 
 var (
-	DefaultConfigMainnet *SufficientChainConfig
-	DefaultConfigMorden  *SufficientChainConfig
+	DefaultConfigMainnet             *SufficientChainConfig
+	DefaultConfigMorden              *SufficientChainConfig
+	TestConfigFrontier               *SufficientChainConfig
+	TestConfigHomestead              *SufficientChainConfig
+	TestConfigEIP150                 *SufficientChainConfig
+	TestConfigFrontierToHomesteadAt5 *SufficientChainConfig
+	TestConfigHomesteadToEIP150At5   *SufficientChainConfig
 )
 
 func init() {
@@ -20,5 +25,27 @@ func init() {
 	DefaultConfigMorden, err = parseExternalChainConfig("/core/config/morden.json", assetsOpen)
 	if err != nil {
 		glog.Fatal("Error parsing morden defaults from JSON:", err)
+	}
+
+	// Test configs for tests/BlockchainTests suite (JSON tests)
+	TestConfigFrontier, err = parseExternalChainConfig("/core/config/test_frontier.json", assetsOpen)
+	if err != nil {
+		glog.Fatal("Error parsing mainnet defaults from JSON:", err)
+	}
+	TestConfigHomestead, err = parseExternalChainConfig("/core/config/test_homestead.json", assetsOpen)
+	if err != nil {
+		glog.Fatal("Error parsing mainnet defaults from JSON:", err)
+	}
+	TestConfigEIP150, err = parseExternalChainConfig("/core/config/test_eip150.json", assetsOpen)
+	if err != nil {
+		glog.Fatal("Error parsing mainnet defaults from JSON:", err)
+	}
+	TestConfigFrontierToHomesteadAt5, err = parseExternalChainConfig("/core/config/test_frontier_to_homestead_at_5.json", assetsOpen)
+	if err != nil {
+		glog.Fatal("Error parsing mainnet defaults from JSON:", err)
+	}
+	TestConfigHomesteadToEIP150At5, err = parseExternalChainConfig("/core/config/test_homestead_to_eip150_at_5.json", assetsOpen)
+	if err != nil {
+		glog.Fatal("Error parsing mainnet defaults from JSON:", err)
 	}
 }
