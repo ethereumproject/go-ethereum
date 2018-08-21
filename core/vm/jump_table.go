6ce5b6c534bf52f8f3ea819bbef7e71a93d4da40
ia <isaac.ardis@gmail.com>
2018-08-21 17:31:41 -0500

    Implement RETURNDATASIZE and RETURNDATACOPY

--- core/vm/jump_table.go
+++ core/vm/jump_table.go
@@ -19,17 +19,10 @@ package vm
 import "math/big"
 
 type jumpPtr struct {
-<<<<<<< HEAD
 	fn    instrFn
 	valid bool
-=======
-	fn     instrFn
-	valid  bool
-	writes bool
->>>>>>> Implement RETURNDATASIZE and RETURNDATACOPY
 	// returns determines whether the operation sets the return data
 	returns bool
-	reverts bool // determines whether the operation reverts state (implicitly halts)
 }
 
 type vmJumpTable [256]jumpPtr
@@ -57,16 +50,9 @@ func newJumpTable(ruleset RuleSet, blockNumber *big.Int) vmJumpTable {
 			valid: true,
 		}
 		jumpTable[RETURNDATACOPY] = jumpPtr{
-			// This is called manually during EVM.Run in order to do error handling in case return data size is out of bounds.
+			// This is called manually during VM.Run in order to do error handling in case return data size is out of bounds.
 			// fn:    opReturnDataCopy,
 			valid: true,
-		}
-		jumpTable[REVERT] = jumpPtr{
-			// This is called manually during EVM.Run since implicity halt (akin to RETURN).
-			fn:      nil,
-			valid:   true,
-			reverts: true,
-			returns: true,
 		}
 	}
 
@@ -281,11 +267,7 @@ func newJumpTable(ruleset RuleSet, blockNumber *big.Int) vmJumpTable {
 	jumpTable[CREATE] = jumpPtr{
 		fn:      opCreate,
 		valid:   true,
-<<<<<<< HEAD
 		valid:   true,
-=======
-		writes:  true,
->>>>>>> Implement RETURNDATASIZE and RETURNDATACOPY
 		returns: true,
 	}
 	jumpTable[CALL] = jumpPtr{
