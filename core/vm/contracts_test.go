95c869cc9557f2b1303bba3db467c03de9e74efc
ia <isaac.ardis@gmail.com>
2018-08-23 19:18:30 -0500

    :hammer:

--- core/vm/contracts_test.go
+++ core/vm/contracts_test.go
@@ -340,7 +340,7 @@ func testPrecompiled(addr string, test precompiledTest, t *testing.T) {
 	p := PrecompiledContractsECIP1045[common.HexToAddress(addr).Str()]
 	in := common.Hex2Bytes(test.input)
 	contract := NewContract(AccountRef(common.HexToAddress("1337")),
-		nil, new(big.Int), p.RequiredGas(in))
+		nil, new(big.Int), p.Gas(in))
 	t.Run(fmt.Sprintf("%s-Gas=%d", test.name, contract.Gas), func(t *testing.T) {
 		if res, err := RunPrecompiledContract(p, in, contract); err != nil {
 			t.Error(err)
@@ -354,9 +354,9 @@ func benchmarkPrecompiled(addr string, test precompiledTest, bench *testing.B) {
 	if test.noBenchmark {
 		return
 	}
-	p := PrecompiledContractsECIP1045[common.HexToAddress(addr)]
+	p := PrecompiledContractsECIP1045[common.HexToAddress(addr).Str()]
 	in := common.Hex2Bytes(test.input)
-	reqGas := p.RequiredGas(in)
+	reqGas := p.Gas(in)
 	contract := NewContract(AccountRef(common.HexToAddress("1337")),
 		nil, new(big.Int), reqGas)
 
